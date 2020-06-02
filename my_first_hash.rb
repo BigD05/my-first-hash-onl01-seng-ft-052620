45ba83dfb6413df1f446f7be1a96828ebf19e9af
kyle Dornan <kyledornan26@gmail.com>
2020-06-02 16:56:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,16 +26,10 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+                  "oil paintings" => 3,
+                  "musket"=>2,
+                "gun powder"=>4,
+  }
+  shipping_manifest
+  
 end
